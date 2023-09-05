b7dd156eb7c387ad6b4b207036a611d90789f91d
Will Anderson <hopplew@gmail.com>
2023-09-05 18:42:20 -0400

    added code and final file

--- reviews.py
+++ reviews.py
@@ -1,2 +1,14 @@
-# add your code here
+import pandas as pd
+pd.set_option('display.max_rows', 5)
+import numpy as np
+wine_data = pd.read_csv("data/winemag-data-130k-v2.csv.zip", index_col=0)
+
+country_count = wine_data.country.value_counts()
+avg_points = wine_data.groupby(['country']).points.mean().round(1)
+
+wine_matrix = pd.concat([country_count, avg_points], axis=1).rename(columns={'country' : 'count'})
+
+wine_matrix
+
+wine_matrix.to_csv(r'data\reviews-per-country.csv')
 
